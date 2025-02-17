40ba52b110df570b9eac683b8cb0831c015462e6
Dmitriy Bondar <db@siur.org.ua>
2025-02-17 02:38:00 +0200

    refix task 3. forget add log.log

--- dmitryi_bondar/task_3_OS_Linux/bash_test/create_files.sh
+++ dmitryi_bondar/task_3_OS_Linux/bash_test/create_files.sh
@@ -2,7 +2,9 @@
 
 count=0
 while [ $count -lt 5 ]; do
-  echo "This is file $count. file$count.txt"
-  touch "file+$count.txt"
+  echo "This is file $count. file$count.txt" >> log.log
+  touch "file$count.txt"
   let count=$count+1
 done
+
+cat ./log.log
