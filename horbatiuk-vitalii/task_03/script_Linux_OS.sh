fae3b3851f17e57eee092e17179bf70218179a20
vitaljiano <vitaljiano@gmail.com>
2025-02-17 18:35:51 +0000

    modefited script

--- horbatiuk-vitalii/task_03/script_Linux_OS.sh
+++ horbatiuk-vitalii/task_03/script_Linux_OS.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 LOG_FILE="/my_files/backup.log"
 
 SOURCE="$1"
@@ -7,7 +9,7 @@ REMOTE_HOST="root@172.30.2.2"
 
 echo "$(date): Start copying..." | tee -a "$LOG_FILE"
 
-EXCLUDED_FOLDERS=$(ssh "$REMOTE_HOST" "find '$DESTINATION' -type d -exec du -s {} + | awk '\$1 > $SIZE_THR>
+EXCLUDED_FOLDERS=$(ssh "$REMOTE_HOST" "find '$DESTINATION' -type d -exec du -s {} + | awk '\$1 > $SIZE_THRESHOLD {print \$2}'")
 if [ -n "$EXCLUDED_FOLDERS" ]; then
     echo "$(date): Oversized folders found: $EXCLUDED_FOLDERS" | tee -a "$LOG_FILE"
 else
@@ -19,13 +21,12 @@ for folder in $EXCLUDED_FOLDERS; do
     EXCLUDE_OPTS+=" --exclude='$folder'"
 done
 RSYNC_COMMAND="rsync -av --delete $EXCLUDE_OPTS '$SOURCE' '$DESTINATION'"
-
 echo "$(date): command executing: $RSYNC_COMMAND" | tee -a "$LOG_FILE"
 eval $RSYNC_COMMAND 2>> "$LOG_FILE"
 
 if [ "$?" -eq 0 ]; then
     echo "$(date): Backup completed successfully!" | tee -a "$LOG_FILE"
-	
-	else
+
+        else
     echo "$(date): Error during backup!" | tee -a "$LOG_FILE"
 fi
