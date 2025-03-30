1ce2ee09e7c3a649181edbc854feed75ba83401d
ivansstef <ivansstef@gmail.com>
2025-03-30 08:58:43 +0000

    Add Task_14 with ISavytskyy project

---
+++ Task_14/ISavytskyy/ui/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/style.css', to: 'css/style.css' },
+      ],
+    }),
+  ],
+  mode: 'development',
+};
